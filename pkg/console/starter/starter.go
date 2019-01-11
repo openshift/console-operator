e338bcc07b9b5fb916fa917d73cf28cf4457e1d7
Benjamin A. Petersen <ben@benjaminapetersen.me>
2019-01-11 14:39:59 -0500

    Update pkg/starter to use v1/Console

--- pkg/console/starter/starter.go
+++ pkg/console/starter/starter.go
@@ -1,21 +1,24 @@
 package starter
 
 import (
+	// standard
 	"fmt"
 	"time"
 
-	"github.com/openshift/console-operator/pkg/api"
-	"github.com/openshift/library-go/pkg/controller/controllercmd"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	// k8s
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
-	"k8s.io/client-go/tools/cache"
-
-	// "k8s.io/client-go/dynamic"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 
-	authclient "github.com/openshift/client-go/oauth/clientset/versioned"
+	// openshift
+
+	"github.com/openshift/console-operator/pkg/api"
+	"github.com/openshift/library-go/pkg/controller/controllercmd"
+
 	// clients
+	// configv1client "github.com/openshift/client-go/config/clientset/versioned"
+	authclient "github.com/openshift/client-go/oauth/clientset/versioned"
 	routesclient "github.com/openshift/client-go/route/clientset/versioned"
 	"github.com/openshift/console-operator/pkg/generated/clientset/versioned"
 
@@ -25,29 +28,19 @@ import (
 	"github.com/openshift/console-operator/pkg/generated/informers/externalversions"
 
 	// operator
-	operatorv1alpha1 "github.com/openshift/api/operator/v1alpha1"
 	"github.com/openshift/console-operator/pkg/console/operator"
 )
 
 func RunOperator(ctx *controllercmd.ControllerContext) error {
-	// TODO: reenable this after upgradeing library-go
-	// only for the ClusterStatus, everything else has a specific client
-	//dynamicClient, err := dynamic.NewForConfig(ctx.KubeConfig)
+	//configClient, err := configv1client.NewForConfig(ctx.KubeConfig)
 	//if err != nil {
 	//	return err
 	//}
-
-	// creates a new kube clientset
-	// ctx.KubeConfig is a REST config
-	// a clientSet contains clients for groups.
-	// each group has one version included in the set.
 	kubeClient, err := kubernetes.NewForConfig(ctx.KubeConfig)
 	if err != nil {
 		return err
 	}
 
-	// pkg/apis/console/v1alpha1/types.go has a `genclient` annotation,
-	// that creates the expected functions for the type.
 	consoleOperatorClient, err := versioned.NewForConfig(ctx.KubeConfig)
 	if err != nil {
 		return err
@@ -69,16 +62,15 @@ func RunOperator(ctx *controllercmd.ControllerContext) error {
 	// TODO: can perhaps put this back the way it was, but may
 	// need to create a couple different version for
 	// resources w/different names
-	tweakListOptions := func(options *v1.ListOptions) {
+	tweakListOptions := func(options *metav1.ListOptions) {
 		// options.FieldSelector = fields.OneTermEqualSelector("metadata.name", operator.ResourceName).String()
 	}
 
-	tweakOAuthListOptions := func(options *v1.ListOptions) {
+	tweakOAuthListOptions := func(options *metav1.ListOptions) {
 		options.FieldSelector = fields.OneTermEqualSelector("metadata.name", api.OAuthClientName).String()
 	}
 
 	kubeInformersNamespaced := informers.NewSharedInformerFactoryWithOptions(
-		// takes a client
 		kubeClient,
 		resync,
 		// takes an unlimited number of additional "options" arguments, which are functions,
@@ -115,13 +107,13 @@ func RunOperator(ctx *controllercmd.ControllerContext) error {
 
 	consoleOperator := operator.NewConsoleOperator(
 		// informers
-		consoleOperatorInformers.Console().V1alpha1().Consoles(), // Console
-		kubeInformersNamespaced.Core().V1(),                      // Secrets, ConfigMaps, Service
-		kubeInformersNamespaced.Apps().V1().Deployments(),        // Deployments
-		routesInformersNamespaced.Route().V1().Routes(),          // Route
-		oauthInformers.Oauth().V1().OAuthClients(),               // OAuth clients
+		consoleOperatorInformers.Console().V1().Consoles(), // Console
+		kubeInformersNamespaced.Core().V1(),                // Secrets, ConfigMaps, Service
+		kubeInformersNamespaced.Apps().V1().Deployments(),  // Deployments
+		routesInformersNamespaced.Route().V1().Routes(),    // Route
+		oauthInformers.Oauth().V1().OAuthClients(),         // OAuth clients
 		// clients
-		consoleOperatorClient.ConsoleV1alpha1(),
+		consoleOperatorClient.ConsoleV1(),
 		kubeClient.CoreV1(), // Secrets, ConfigMaps, Service
 		kubeClient.AppsV1(),
 		routesClient.RouteV1(),
@@ -136,38 +128,34 @@ func RunOperator(ctx *controllercmd.ControllerContext) error {
 
 	go consoleOperator.Run(ctx.StopCh)
 
-	// TODO: turn this back on!
-	// for now its just creating noise.... as we need to update library-go for it to work correctly
-	// our version of library-go has the old group
 	//clusterOperatorStatus := status.NewClusterOperatorStatusController(
-	//	controller.TargetNamespace,
-	//	controller.ResourceName,
-	//	// no idea why this is dynamic & not a strongly typed client.
-	//	dynamicClient,
-	//	&operatorStatusProvider{informers: consoleOperatorInformers},
+	//	api.ResourceName,
+	//	configClient.ConfigV1(),
+	//	operatorStatusProvider{informers: consoleOperatorInformers},
+	//	ctx.EventRecorder,
 	//)
-	//// TODO: will have a series of Run() funcs here
-	//go clusterOperatorStatus.Run(1, stopCh)
+
+	//go clusterOperatorStatus.Run(1, ctx.StopCh)
 
 	<-ctx.StopCh
 
 	return fmt.Errorf("stopped")
 }
 
-// I'd prefer this in a /console/status/ package, but other operators keep it here.
-type operatorStatusProvider struct {
-	informers externalversions.SharedInformerFactory
-}
-
-func (p *operatorStatusProvider) Informer() cache.SharedIndexInformer {
-	return p.informers.Console().V1alpha1().Consoles().Informer()
-}
-
-func (p *operatorStatusProvider) CurrentStatus() (operatorv1alpha1.OperatorStatus, error) {
-	instance, err := p.informers.Console().V1alpha1().Consoles().Lister().Consoles(api.TargetNamespace).Get(api.ResourceName)
-	if err != nil {
-		return operatorv1alpha1.OperatorStatus{}, err
-	}
-
-	return instance.Status.OperatorStatus, nil
-}
+//// I'd prefer this in a /console/status/ package, but other operators keep it here.
+//type operatorStatusProvider struct {
+//	informers externalversions.SharedInformerFactory
+//}
+//
+//func (p operatorStatusProvider) Informer() cache.SharedIndexInformer {
+//	return p.informers.Console().V1alpha1().Consoles().Informer()
+//}
+//
+//func (p operatorStatusProvider) CurrentStatus() (v1.OperatorStatus, error) {
+//	instance, err := p.informers.Console().V1alpha1().Consoles().Lister().Consoles(api.TargetNamespace).Get(api.ResourceName)
+//	if err != nil {
+//		return v1.OperatorStatus{}, err
+//	}
+//
+//	return instance.Status.OperatorStatus, nil
+//}
