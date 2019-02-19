f9b9194c2c964ec3cf91b974774268ed18e88cbb
Benjamin A. Petersen <ben@benjaminapetersen.me>
2019-02-19 15:02:47 -0500

    Update sync loop to set Status.PublicURL instead of Status.PublicHostname

--- pkg/console/operator/sync_v400.go
+++ pkg/console/operator/sync_v400.go
@@ -111,7 +111,7 @@ func sync_v400(co *consoleOperator, operatorConfig *operatorv1.Console, consoleC
 
 func SyncConsoleConfig(co *consoleOperator, consoleConfig *configv1.Console, route *routev1.Route) (*configv1.Console, error) {
 	logrus.Printf("Updating console.config.openshift.io with hostname: %v \n", route.Spec.Host)
-	consoleConfig.Status.PublicHostname = route.Spec.Host
+	consoleConfig.Status.PublicURL = route.Spec.Host
 	return co.consoleConfigClient.UpdateStatus(consoleConfig)
 }
 
