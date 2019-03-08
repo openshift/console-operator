31f1d47428d3ac50063bb8acaf82a9d7bf9d8803
Benjamin A. Petersen <ben@benjaminapetersen.me>
2019-03-08 10:37:16 -0500

    Add test for route IsAdmitted()

--- pkg/console/subresource/route/route_test.go
+++ pkg/console/subresource/route/route_test.go
@@ -1,8 +1,11 @@
 package route
 
 import (
-	"github.com/go-test/deep"
 	"testing"
+
+	v1 "k8s.io/api/core/v1"
+
+	"github.com/go-test/deep"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
@@ -106,6 +109,129 @@ func TestStub(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if diff := deep.Equal(Stub(), tt.want); diff != nil {
 				t.Error(diff)
+			}
+		})
+	}
+}
+
+func TestIsAdmitted(t *testing.T) {
+	type args struct {
+		route *routev1.Route
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "Test IsAdmitted(): Route has an ingress with a matching host with an admission status of true.",
+			args: args{
+				route: &routev1.Route{
+					Spec: routev1.RouteSpec{
+						Host: "usethisone.com",
+					},
+					Status: routev1.RouteStatus{
+						Ingress: []routev1.RouteIngress{
+							{
+								Host: "usethisone.com",
+								Conditions: []routev1.RouteIngressCondition{{
+									Type:   routev1.RouteAdmitted,
+									Status: v1.ConditionTrue,
+								}},
+							},
+							{
+								Host: "notthisone.com",
+								Conditions: []routev1.RouteIngressCondition{{
+									Type:   routev1.RouteAdmitted,
+									Status: v1.ConditionFalse,
+								}},
+							},
+						},
+					},
+				},
+			},
+			// we do have a matching ingress that has been admitted
+			want: true,
+		},
+		{
+			name: "Test IsAdmitted(): Route has an ingress with a matching host but with an admission status of false.",
+			args: args{
+				route: &routev1.Route{
+					Spec: routev1.RouteSpec{
+						Host: "usethisone.com",
+					},
+					Status: routev1.RouteStatus{
+						Ingress: []routev1.RouteIngress{
+							{
+								Host: "usethisone.com",
+								Conditions: []routev1.RouteIngressCondition{{
+									Type:   routev1.RouteAdmitted,
+									Status: v1.ConditionFalse,
+								}},
+							},
+							{
+								Host: "notthisone.com",
+								Conditions: []routev1.RouteIngressCondition{{
+									Type:   routev1.RouteAdmitted,
+									Status: v1.ConditionFalse,
+								}},
+							},
+						},
+					},
+				},
+			},
+			// in this case, the function should return false
+			want: false,
+		}, {
+			name: "Test IsAdmitted(): Route has no matching ingress.",
+			args: args{
+				route: &routev1.Route{
+					Spec: routev1.RouteSpec{
+						Host: "usethisone.com",
+					},
+					Status: routev1.RouteStatus{
+						Ingress: []routev1.RouteIngress{},
+					},
+				},
+			},
+			// no matching ingress, so we should return false
+			want: false,
+		}, {
+			name: "Test IsAdmitted(): Route has no ingress with a matching host.",
+			args: args{
+				route: &routev1.Route{
+					Spec: routev1.RouteSpec{
+						Host: "usethisone.com",
+					},
+					Status: routev1.RouteStatus{
+						Ingress: []routev1.RouteIngress{
+							{
+								Host: "nope.com",
+								Conditions: []routev1.RouteIngressCondition{{
+									Type:   routev1.RouteAdmitted,
+									Status: v1.ConditionFalse,
+								}},
+							},
+							{
+								Host: "nopenope.com",
+								Conditions: []routev1.RouteIngressCondition{{
+									Type:   routev1.RouteAdmitted,
+									Status: v1.ConditionFalse,
+								}},
+							},
+						},
+					},
+				},
+			},
+			// no ingress with a matching host, so we should return false
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsAdmitted(tt.args.route); got != tt.want {
+				t.Errorf("IsAdmitted() = \n%v\n want \n%v", got, tt.want)
 			}
 		})
 	}
