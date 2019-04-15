30170c4a4469d68f1696b8bbe8b743eda6afde57
Benjamin A. Petersen <ben@benjaminapetersen.me>
2019-04-15 13:46:29 -0400

    Remove service-ca-operator from /vendor

--- vendor/github.com/openshift/service-ca-operator/pkg/boilerplate/operator/filter.go
+++
@@ -1,7 +0,0 @@
-package operator
-
-import "github.com/openshift/service-ca-operator/pkg/boilerplate/controller"
-
-func FilterByNames(names ...string) controller.Filter {
-	return controller.FilterByNames(nil, names...)
-}
--- vendor/github.com/openshift/service-ca-operator/pkg/operator/operatorclient/interfaces.go
+++
@@ -1,8 +0,0 @@
-package operatorclient
-
-const (
-	GlobalUserSpecifiedConfigNamespace    = "openshift-config"
-	GlobalMachineSpecifiedConfigNamespace = "openshift-config-managed"
-	OperatorNamespace                     = "openshift-service-ca-operator"
-	TargetNamespace                       = "openshift-service-ca"
-)
