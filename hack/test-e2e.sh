2ce3053703aa8e02ff6bce32b00ab533b1308b4a
Jakub Hadvig <jhadvig@redhat.com>
2019-05-06 10:53:36 +0200

    Test PR

--- hack/test-e2e.sh
+++ hack/test-e2e.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source "$(dirname "${BASH_SOURCE}")/lib/init.sh"
 
+go version
 os::log::info "Running e2e tests"
 KUBERNETES_CONFIG=${KUBECONFIG} GOCACHE=off go test -timeout 30m -v ./test/e2e/
