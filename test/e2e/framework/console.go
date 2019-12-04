936c0a22aea416305e21085e6903ccff997139cb
Benjamin A. Petersen <ben@benjaminapetersen.me>
2019-12-04 11:10:03 -0500

    f

--- test/e2e/framework/console.go
+++ test/e2e/framework/console.go
@@ -6,7 +6,8 @@ import (
 	"testing"
 	"time"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 
 	"github.com/openshift/console-operator/pkg/api"
@@ -25,16 +26,19 @@ const (
 // of replicas have been created and are running and healthy.
 func ConsolePodsMustSettle(t *testing.T, clientset *ClientSet) {
 	count := 0
+	t.Log("ConsolePodsMustSettle()")
 	err := wait.Poll(pollFrequency, pollStandardMax, func() (stop bool, err error) {
 		count++
 		t.Log(fmt.Sprintf("running %d time(s)", count))
-		err, ok := ConsolePodsRunning(clientset)
-		t.Logf(fmt.Sprintf("is ok: %t, why? %s", ok, err))
+		err, ok := ConsolePodsRunning(t, clientset)
+		t.Logf(fmt.Sprintf("is ok: %t, because:%s", ok, err))
 		if err != nil {
-			return ok, err
+			t.Log("continue due to error")
+			return false, err
 		}
+		t.Log("stop, no error")
 		// Try until timeout... can we just return both?
-		return ok, nil
+		return true, nil
 	})
 
 	if err != nil {
@@ -43,41 +47,51 @@ func ConsolePodsMustSettle(t *testing.T, clientset *ClientSet) {
 	}
 }
 
-func ConsolePodsRunning(clientset *ClientSet) (err error, ok bool) {
-	deployment, err := clientset.Apps.Deployments(api.OpenShiftConsoleNamespace).Get(api.OpenShiftConsoleName, v1.GetOptions{})
+func ConsolePodsRunning(t *testing.T, clientset *ClientSet) (err error, ok bool) {
+	deployment, err := clientset.Apps.Deployments(api.OpenShiftConsoleNamespace).Get(api.OpenShiftConsoleName, metav1.GetOptions{})
 	if err != nil {
+		t.Fatal("GET deployment error")
 		return err, false
 	}
 
 	// if all is well, exit early
 	if deploymentsub.AllPodsHealthy(deployment) {
+		t.Log("All pods healthy")
 		return nil, true
 	}
+	// otherwise, lets report whats wrong
+	fmt.Printf("%d/%d replicas ready of expected %d\n", deployment.Status.ReadyReplicas, deployment.Status.Replicas, deployment.Status.Replicas)
 
-	fmt.Printf("something wrong, %d/%d replicas ready of expected %d\n", deployment.Status.ReadyReplicas, deployment.Status.Replicas, deployment.Status.Replicas)
-
-	if deployment.Status.Replicas > 2 {
-		// todo: may or may not be a bad thing, we need to investigate further
-		//    this may simply indicate churn, new pods are rolling out, old pods being terminated
-		//    do we need to consider this?
-	}
-	pods, err := clientset.Core.Pods(api.OpenShiftConsoleNamespace).List(v1.ListOptions{
+	pods, err := clientset.Core.Pods(api.OpenShiftConsoleNamespace).List(metav1.ListOptions{
 		LabelSelector: "app=console,component=ui",
 	})
 	if err != nil {
+		t.Fatal("GET pods error")
 		return err, false
 	}
 
 	for _, pod := range pods.Items {
+		t.Log(fmt.Sprintf("pod %s is %s", pod.Name, pod.Status.Phase))
+		if pod.Status.Phase == corev1.PodRunning {
+			continue
+		}
+		t.Log(fmt.Sprintf(">> Pod %s is %s\n", pod.Name, pod.Status.Phase))
+
 		for _, status := range pod.Status.ContainerStatuses {
+			// TODO: remove, just logging for checks
 			if status.State.Running != nil {
-				fmt.Printf("container %s is running\n", status.Name)
+				t.Log(fmt.Sprintf(">>> container %s is running\n", status.Name))
 			}
+			// TODO: remove, just logging for checks
 			if status.State.Terminated != nil {
-				fmt.Printf("container %s is terminated\n", status.Name)
+				t.Log(fmt.Sprintf(">>> container %s is terminated\n", status.Name))
 			}
 			if status.State.Waiting != nil {
-				fmt.Printf("container %s is %s because %s\n", status.Name, status.State.Waiting.Reason, status.State.Waiting.Message)
+				if len(status.State.Waiting.Message) != 0 {
+					t.Log(fmt.Sprintf(">>> container %s is %s because %s\n", status.Name, status.State.Waiting.Reason, status.State.Waiting.Message))
+				} else {
+					t.Log(fmt.Sprintf(">>> container %s is %s\n", status.Name, status.State.Waiting.Reason))
+				}
 				return errors.New(status.State.Waiting.Message), false
 			}
 		}
