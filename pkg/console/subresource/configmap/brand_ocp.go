b5fb7651fede2bcad9dc86378b28d7a3c292c2e4
Yaakov Selkowitz <yselkowi@redhat.com>
2021-11-01 10:41:42 -0400

    Migrate docs links to access.redhat.com

--- pkg/console/subresource/configmap/brand_ocp.go
+++ pkg/console/subresource/configmap/brand_ocp.go
@@ -4,5 +4,5 @@ package configmap
 
 const (
 	DEFAULT_BRAND   = "ocp"
-	DEFAULT_DOC_URL = "https://docs.openshift.com/container-platform/4.9/"
+	DEFAULT_DOC_URL = "https://access.redhat.com/documentation/en-us/openshift_container_platform/4.9/"
 )
