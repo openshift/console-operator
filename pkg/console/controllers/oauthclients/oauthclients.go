9866e0563e0fa368da868ad5be55be7f4703c910
Ilias Rinis <irinis@redhat.com>
2023-12-11 15:22:36 +0100

    oauthclients: add an informer stop/start method

--- pkg/console/controllers/oauthclients/oauthclients.go
+++ pkg/console/controllers/oauthclients/oauthclients.go
@@ -3,6 +3,7 @@ package oauthclients
 import (
 	"context"
 	"fmt"
+	"time"
 
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -10,11 +11,14 @@ import (
 	corev1informers "k8s.io/client-go/informers/core/v1"
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 	corev1listers "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/klog/v2"
 
+	configv1 "github.com/openshift/api/config/v1"
 	operatorv1 "github.com/openshift/api/operator/v1"
 	configv1informers "github.com/openshift/client-go/config/informers/externalversions/config/v1"
 	configv1lister "github.com/openshift/client-go/config/listers/config/v1"
+	oauthclient "github.com/openshift/client-go/oauth/clientset/versioned"
 	oauthv1client "github.com/openshift/client-go/oauth/clientset/versioned/typed/oauth/v1"
 	oauthv1informers "github.com/openshift/client-go/oauth/informers/externalversions/oauth/v1"
 	oauthv1lister "github.com/openshift/client-go/oauth/listers/oauth/v1"
@@ -41,38 +45,49 @@ type oauthClientsController struct {
 	secretsClient  corev1client.SecretsGetter
 
 	oauthClientLister     oauthv1lister.OAuthClientLister
+	authnLister           configv1lister.AuthenticationLister
 	consoleOperatorLister operatorv1listers.ConsoleLister
 	routesLister          routev1listers.RouteLister
 	ingressConfigLister   configv1lister.IngressLister
 	targetNSSecretsLister corev1listers.SecretLister
+
+	oauthClientsInformerSwitch *informerWithSwitch
 }
 
 func NewOAuthClientsController(
+	ctx context.Context,
 	operatorClient v1helpers.OperatorClient,
-	oauthClient oauthv1client.OAuthClientsGetter,
+	oauthClient oauthclient.Interface,
 	secretsClient corev1client.SecretsGetter,
-	oauthClientInformer oauthv1informers.OAuthClientInformer,
+	authnInformer configv1informers.AuthenticationInformer,
 	consoleOperatorInformer operatorv1informers.ConsoleInformer,
 	routeInformer routev1informers.RouteInformer,
 	ingressConfigInformer configv1informers.IngressInformer,
 	targetNSsecretsInformer corev1informers.SecretInformer,
 	recorder events.Recorder,
 ) factory.Controller {
+	oauthClientInformer := oauthv1informers.NewOAuthClientInformer(oauthClient, 10*time.Minute, nil)
+
 	c := oauthClientsController{
-		oauthClient:    oauthClient,
+		oauthClient:    oauthClient.OauthV1(),
 		operatorClient: operatorClient,
 		secretsClient:  secretsClient,
 
-		oauthClientLister:     oauthClientInformer.Lister(),
+		oauthClientLister:     oauthv1lister.NewOAuthClientLister(oauthClientInformer.GetIndexer()),
+		authnLister:           authnInformer.Lister(),
 		consoleOperatorLister: consoleOperatorInformer.Lister(),
 		routesLister:          routeInformer.Lister(),
 		ingressConfigLister:   ingressConfigInformer.Lister(),
 		targetNSSecretsLister: targetNSsecretsInformer.Lister(),
+
+		oauthClientsInformerSwitch: NewOAuthClientsInformerSwitch(ctx, oauthClientInformer),
 	}
+	defer c.oauthClientsInformerSwitch.EnsureRunning()
 
 	return factory.New().
 		WithSync(c.sync).
 		WithInformers(
+			authnInformer.Informer(),
 			consoleOperatorInformer.Informer(),
 			routeInformer.Informer(),
 			ingressConfigInformer.Informer(),
@@ -80,7 +95,7 @@ func NewOAuthClientsController(
 		).
 		WithFilteredEventsInformers(
 			factory.NamesFilter(api.OAuthClientName),
-			oauthClientInformer.Informer(),
+			oauthClientInformer,
 		).
 		WithSyncDegradedOnError(operatorClient).
 		ToController("OAuthClientsController", recorder.WithComponentSuffix("oauth-clients-controller"))
@@ -105,6 +120,11 @@ func (c *oauthClientsController) sync(ctx context.Context, controllerContext fac
 		return err
 	}
 
+	authnConfig, err := c.authnLister.Get(api.ConfigResourceName)
+	if err != nil {
+		return err
+	}
+
 	routeName := api.OpenShiftConsoleRouteName
 	routeConfig := routesub.NewRouteConfig(operatorConfig, ingressConfig, routeName)
 	if routeConfig.IsCustomHostnameSet() {
@@ -116,16 +136,34 @@ func (c *oauthClientsController) sync(ctx context.Context, controllerContext fac
 		return routeErr
 	}
 
-	clientSecret, secErr := c.syncSecret(ctx, operatorConfig, controllerContext.Recorder())
-	statusHandler.AddConditions(status.HandleProgressingOrDegraded("OAuthClientSecretSync", "FailedApply", secErr))
-	if secErr != nil {
-		return statusHandler.FlushAndReturn(secErr)
+	switch authnConfig.Spec.Type {
+	case "", configv1.AuthenticationTypeIntegratedOAuth:
+		c.oauthClientsInformerSwitch.EnsureRunning()
+
+		waitCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+		defer cancel()
+		if !cache.WaitForCacheSync(waitCtx.Done(), c.oauthClientsInformerSwitch.informer.HasSynced) {
+			return fmt.Errorf("timed out waiting for OAuthClients cache sync")
+		}
+
+		clientSecret, secErr := c.syncSecret(ctx, operatorConfig, controllerContext.Recorder())
+		statusHandler.AddConditions(status.HandleProgressingOrDegraded("OAuthClientSecretSync", "FailedApply", secErr))
+		if secErr != nil {
+			return statusHandler.FlushAndReturn(secErr)
+		}
+
+		oauthErrReason, oauthErr := c.syncOAuthClient(ctx, clientSecret, consoleURL.String())
+		statusHandler.AddConditions(status.HandleProgressingOrDegraded("OAuthClientSync", oauthErrReason, oauthErr))
+		if oauthErr != nil {
+			return statusHandler.FlushAndReturn(oauthErr)
+		}
+		return nil
+	case "OIDC":
+		c.oauthClientsInformerSwitch.Stop()
+		return nil
+	default:
+		return nil
 	}
-
-	oauthErrReason, oauthErr := c.syncOAuthClient(ctx, clientSecret, consoleURL.String())
-	statusHandler.AddConditions(status.HandleProgressingOrDegraded("OAuthClientSync", oauthErrReason, oauthErr))
-
-	return statusHandler.FlushAndReturn(oauthErr)
 }
 
 // handleStatus returns whether sync should happen and any error encountering
@@ -197,4 +235,37 @@ func (c *oauthClientsController) deregisterClient(ctx context.Context) error {
 	_, err = c.oauthClient.OAuthClients().Update(ctx, updated, metav1.UpdateOptions{})
 	return err
 
+}
+
+type informerWithSwitch struct {
+	informer  cache.SharedIndexInformer
+	parentCtx context.Context
+	runCtx    context.Context
+	stopFunc  func()
+}
+
+func NewOAuthClientsInformerSwitch(ctx context.Context, informer cache.SharedIndexInformer) *informerWithSwitch {
+	return &informerWithSwitch{
+		informer:  informer,
+		parentCtx: ctx,
+	}
+}
+
+func (s *informerWithSwitch) EnsureRunning() {
+	if s.runCtx != nil {
+		return
+	}
+
+	s.runCtx, s.stopFunc = context.WithCancel(s.parentCtx)
+	go s.informer.Run(s.runCtx.Done())
+}
+
+func (s *informerWithSwitch) Stop() {
+	if s.runCtx == nil {
+		return
+	}
+
+	s.stopFunc()
+	s.runCtx = nil
+	s.stopFunc = nil
 }
