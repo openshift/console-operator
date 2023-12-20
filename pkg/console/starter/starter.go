7b721f82911f66cec5ac6a213e3217598f668b00
Ilias Rinis <irinis@redhat.com>
2023-12-20 11:54:44 +0100

    oauthclients: sync oauth client secret from OIDC configuration for auth type OIDC

--- pkg/console/starter/starter.go
+++ pkg/console/starter/starter.go
@@ -214,6 +214,7 @@ func RunOperator(ctx context.Context, controllerContext *controllercmd.Controlle
 		routesInformersNamespaced.Route().V1().Routes(),
 		configInformers.Config().V1().Ingresses(),
 		kubeInformersNamespaced.Core().V1().Secrets(),
+		kubeInformersConfigNamespaced.Core().V1().Secrets(),
 		recorder,
 	)
 
