635a4160b0cd1745bfb4596ddcd15fc2f386c268
Jakub Hadvig <jhadvig@redhat.com>
2024-05-20 15:41:50 +0200

    OCPBUGS-33715: Cache organization ID

--- pkg/console/telemetry/telemetry.go
+++ pkg/console/telemetry/telemetry.go
@@ -12,6 +12,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	appsv1listers "k8s.io/client-go/listers/apps/v1"
 	v1 "k8s.io/client-go/listers/core/v1"
+	"k8s.io/klog/v2"
 
 	"github.com/openshift/console-operator/pkg/api"
 	deploymentsub "github.com/openshift/console-operator/pkg/console/subresource/deployment"
@@ -79,6 +80,27 @@ func GetAccessToken(secretsLister v1.SecretLister) (string, error) {
 	return authsBytes.Auth, nil
 }
 
+// check if:
+// 1. custom ORGANIZATION_ID is awailable as telemetry annotation on console-operator config or in telemetry-config configmap
+// 2. cached ORGANIZATION_ID is available on the operator controller instance
+// else fetch the ORGANIZATION_ID from OCM
+func GetOrganizationID(telemetryConfig map[string]string, cachedOrganizationID, clusterID, accessToken string) string {
+	if customOrganizationID, isCustomOrgIDSet := telemetryConfig["ORGANIZATION_ID"]; isCustomOrgIDSet {
+		return customOrganizationID
+	}
+
+	if cachedOrganizationID != "" {
+		return cachedOrganizationID
+	}
+
+	fetchedOrganizationID, err := FetchOrganizationID(clusterID, accessToken)
+	klog.V(4).Infoln("Fetching ORGANIZATION_ID from OCM")
+	if err != nil {
+		klog.Errorf("telemetry config error: %s", err)
+	}
+	return fetchedOrganizationID
+}
+
 // Needed to create our own types for OCM Subscriptions since their types and client are useless
 // https://github.com/openshift-online/ocm-sdk-go/blob/main/accountsmgmt/v1/subscription_client.go - everything private
 // https://github.com/openshift-online/ocm-sdk-go/blob/main/accountsmgmt/v1/subscriptions_client.go#L38-L41 - useless client
@@ -89,8 +111,8 @@ type Organization struct {
 	OrganizationID string `json:"organization_id"`
 }
 
-// GetOrganizationID fetches the organization ID using the cluster ID and access token
-func GetOrganizationID(clusterID, accessToken string) (string, error) {
+// FetchOrganizationID fetches the organization ID using the cluster ID and access token
+func FetchOrganizationID(clusterID, accessToken string) (string, error) {
 	u, err := buildURL(clusterID)
 	if err != nil {
 		return "", err // more contextual error handling can be added here if needed
